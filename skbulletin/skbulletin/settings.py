3ad68c6b76c56b0fe9d211a818d2f0546252e7b8
RYZZACADZ <ryzzacadz@gmail.com>
2025-05-22 14:18:35 +0800

    Switch to SQLite database only

--- skbulletin/skbulletin/settings.py
+++ skbulletin/skbulletin/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import dj_database_url
 from dotenv import load_dotenv
 
 # Load environment variables
@@ -69,20 +68,12 @@ WSGI_APPLICATION = 'skbulletin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/skbulletin',
-            conn_max_age=600
-        )
-    }
+}
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
